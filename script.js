5d7aebdc61e2fe3aeac90b8a58e4842d3f4a1f54
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-05-01 21:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let select =document.getElementById("colorSelect");
+    let inputBtn = document.querySelector("input");
+    
+   // select.addEventListener("change", function selectAndRemove(event_details){
+        //let selectedColor = select.options[select.selectedIndex];
+        
+        inputBtn.addEventListener("click",removeColor);
+        function removeColor(){
+            select.remove(select.selectedIndex);
+        }
